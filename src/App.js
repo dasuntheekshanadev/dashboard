d89eed291a5f09a8bf0da2b51c10b70cb4cc8a7e
Dasun Theekshana <40088028+dasuntheekshanadev@users.noreply.github.com>
2024-03-27 10:23:58 +0530

    you are a hoe

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/Dashboard';
 import Data from './components/Data';
 import NavBar from './components/NavBar';
 import AboutUs from './components/AboutUs';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/about" element={<AboutUs />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
+// Import images for team members
+import IniyanImage from '../assets/iniyan.jpg';
+import IndiraImage from '../assets/indira.jpg';
+import AnshathImage from '../assets/anshath.jpg';
+import KavinduImage from '../assets/kavindu.jpg';
+import BiyoniImage from '../assets/biyoni.jpg';
+
 const AboutUs = () => {
+  // Define team members data
+  const teamMembers = [
+    { name: 'Iniyan Manimaran', image: IniyanImage },
+    { name: 'Indira Liyanarachchi', image: IndiraImage },
+    { name: 'Anshath Ahamedd', image: AnshathImage }, 
+    { name: 'Kavindu Janojya', image: KavinduImage },
+    { name: 'Biyoni Ranathunga', image: BiyoniImage },
+  ];
+
   return (
-    <div>
-      <h1>About Us Page</h1>
-      {/* Add your content here */}
+    <div className="bg-gradient-to-r from-blue-600 to-blue-900 min-h-screen flex flex-col text-white">
+      <h1 className="text-2xl font-bold p-4">Meet Our Team</h1>
+      <p className="font-bold text-2xl text-center text-gray-200 px-6 py-4">
+        We are a team of passionate developers working on a university project - A Home Automation System. Our aim is to create a smart environment that enhances convenience, security, and energy efficiency for homeowners.
+      </p>
+      <div className="flex-grow flex justify-center items-center flex-wrap">
+        {teamMembers.map((member, index) => (
+          <div key={index} className="max-w-sm mx-4 mb-4 bg-white rounded-lg overflow-hidden shadow-lg">
+            <img className="w-full h-auto" style={{ maxWidth: '200px' }} src={member.image} alt={member.name} />
+            <div className="p-4">
+              <p className="text-gray-800 text-lg font-semibold">{member.name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
